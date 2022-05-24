1a47a73f92351dfe221600f0e8f148695e6233bb
geicibarham <geicianecosta43@gmail.com>
2022-05-23 22:08:24 -0400

    added tests and started working on cart funcitonality

--- client/src/utils/actions.js
+++ client/src/utils/actions.js
@@ -8,4 +8,4 @@ export const ADD_MULTIPLE_TO_CART = ' ADD_MULTIPLE_TO_CART';
 export const REMOVE_FROM_CART = ' REMOVE_FROM_CART';
 export const UPDATE_CART_QUANTITY = 'UPDATE_CART_QUANTITY';
 export const CLEAR_CART = ' CLEAR_CART ';
-export const AOGGLE_CART = ' TOGGLE_CART';
+export const TOGGLE_CART = ' TOGGLE_CART';
